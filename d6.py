71d154887512122478f008d1e5fc7b8aeae027d7
ika1994 <veselinovic.ilija250@gmail.com>
2024-09-05 00:01:13 +0200

    added grade

--- d6.py
+++ d6.py
@@ -1,8 +1,18 @@
 students=[
     {"name":"Toma","score":100, "active":True},
-    {"name":"Milos","score":20, "active":True},
+    {"name":"Milos","score":35, "active":True},
     {"name":"Alija", "score":82, "active":False}
     ]
 for student in students:
     if student["active"]:
+        if student["score"]>80:
+            student["grade"]="A"
+        elif 60<student["score"]<80:
+            student["grade"]="B"
+        elif 40<student["score"]<60:
+            student["grade"]="C"
+        elif 20<student["score"]<40:
+            student["grade"]="D"
+        else:
+            student["grade"]="F"
         print(student)
