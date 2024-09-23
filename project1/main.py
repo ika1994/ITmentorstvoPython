701575ee2e47568b61c424715a7d7a9c6c4b68df
ika1994 <veselinovic.ilija250@gmail.com>
2024-09-23 18:24:37 +0200

    project 1 first commit

---
+++ project1/main.py
@@ -0,0 +1,94 @@
+import pymysql
+import re
+import bcrypt
+
+import pymysql.cursors
+
+
+HOST="localhost"
+USER="root"
+PASSWORD=""
+DATABASE="project1"
+
+#REGEX#
+# dodati drugi uslov regex
+email_pattert=r""
+name_pattern=r""
+password_pattern=r""
+
+
+
+#create connection with sql
+def create_conn():
+    return pymysql.connect(host=HOST,user=USER,password=PASSWORD, database=DATABASE,cursorclass=pymysql.cursors.DictCursor)
+
+
+def check_user_exist(conn, email):
+    with conn.cursor() as cursor:
+        query="SELECT * FROM users WHERE email=%s"
+        cursor.execute(query,email)
+        return cursor.fetchone()
+    
+def login(conn, email):
+    while True:
+        password = input("Unesite lozinku da biste se ulogovali\n")
+        user = verify_password(conn, email, password)
+        if user:
+            print(f"Uspešno ste se ulogovali: {user["first_name"]}")
+            return True
+        else:
+            print("Greška: neispravna lozinka")
+
+def verify_password(conn, email, password):
+    with conn.cursor() as cursor:
+        query = "SELECT * FROM users WHERE password=%s AND email=%s"
+        cursor.execute(query, (password, email))
+        return cursor.fetchone()
+
+def register_user(conn, email, password, name, last_name):
+    with conn.cursor() as cursor:
+        query = "INSERT INTO users (email, password, first_name, last_name) VALUES (%s, %s, %s, %s)"
+        cursor.execute(query, (email, password, first_name, last_name))
+        conn.commit()
+
+def verify_name(name):
+    False
+
+def verify_email(email):
+    None
+
+def main():
+    conn=create_conn()
+
+    email=""
+    user_password=""
+    action=""
+    first_name=""
+    last_name=""
+
+    while email=="":
+        email=input("Unesite email adresu\n")
+        user=check_user_exist(conn,email)
+    if user:
+        login(conn, email)
+    if not user:
+        while action=="":
+            action=input("---REGISTRACIJA---\nDa li zelite da se registrujete [DA/NE]\n").lower()
+            if action=='da':
+                while first_name=="":
+                    first_name=input("Unesite ime\n")
+                    verify_name(first_name)
+                while last_name=="":
+                    last_name=input("unesite prezime \n")
+                while user_password=="" or len(user_password)<8 :
+                    user_password=input("unesite ispravan pass\n")
+                #upis u bazu
+            elif action=='ne':
+                print("cao")
+            else:
+                action=""
+
+
+
+if __name__ == "__main__":
+    main()
